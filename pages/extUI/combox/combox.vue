b6eb04fb9e639148027e0c70164719b55371a4dc
wzhw <1317670129@qq.com>
2021-05-14 16:13:15 +0800

    新增扩展组件

--- pages/extUI/combox/combox.vue
+++ pages/extUI/combox/combox.vue
@@ -1,41 +1,37 @@
 <template>
 	<view class="page">
-		<text class="example-info">数字角标通用来标记重点信息使用，如接受到新消息、有未读消息等</text>
-		<uni-section title="有底色" type="line"></uni-section>
+		<text class="example-info">组合框一般用于可以选择也可以输入的表单项</text>
+		<uni-section title="基本用法" type="line"></uni-section>
 		<view class="example-body">
-			<uni-badge class="uni-badge-left-margin" text="1" />
-			<uni-badge class="uni-badge-left-margin" text="2" type="primary" />
-			<uni-badge class="uni-badge-left-margin" text="34" type="success" />
-			<uni-badge class="uni-badge-left-margin" text="45" type="warning" />
-			<uni-badge class="uni-badge-left-margin" text="123" type="error" />
+			<uni-combox label="所在城市" :candidates="candidates" placeholder="请选择所在城市" v-model="city"></uni-combox>
+			<view class="result-box">
+				<text>所选城市为：{{city}}</text>
+			</view>
 		</view>
-		<uni-section title="无底色" type="line"></uni-section>
+		<uni-section title="设置label宽度" type="line"></uni-section>
 		<view class="example-body">
-			<uni-badge class="uni-badge-left-margin" :inverted="true" text="1" />
-			<uni-badge class="uni-badge-left-margin" :inverted="true" text="2" type="primary" />
-			<uni-badge class="uni-badge-left-margin" :inverted="true" text="34" type="success" />
-			<uni-badge class="uni-badge-left-margin" :inverted="true" text="45" type="warning" />
-			<uni-badge class="uni-badge-left-margin" :inverted="true" text="123" type="error" />
+			<uni-combox label="所在城市" labelWidth="150px" :candidates="candidates" placeholder="请选择所在城市"></uni-combox>
 		</view>
-		<uni-section title="迷你" type="line"></uni-section>
+		<uni-section title="设置无匹配项时的提示语" type="line"></uni-section>
 		<view class="example-body">
-			<uni-badge class="uni-badge-left-margin" text="1" size="small" />
-			<uni-badge class="uni-badge-left-margin" text="2" type="primary" size="small" />
-			<uni-badge class="uni-badge-left-margin" text="34" type="success" size="small" />
-			<uni-badge class="uni-badge-left-margin" text="45" type="warning" size="small" />
-			<uni-badge class="uni-badge-left-margin" text="123" type="error" size="small" />
+			<uni-combox label="所在城市" emptyTips="这里啥都没有" placeholder="请选择所在城市"></uni-combox>
 		</view>
 	</view>
 </template>
 
 <script>
-import uniSection from '../../../components/uni-ui/uni-section'
 	export default {
-		components: {uniSection},
+		components: {},
 		data() {
-			return {};
+			return {
+				candidates: ['北京', '南京', '东京', '武汉', '天津', '上海', '海口'],
+				city: ''
+			}
+		},
+		methods: {
+
 		}
-	};
+	}
 </script>
 
 <style>
@@ -138,18 +134,14 @@ import uniSection from '../../../components/uni-ui/uni-section'
 		background-color: #4ca2ff;
 	}
 
-	/* #ifdef MP-ALIPAY */
-	.uni-badge {
-		margin-left: 20rpx;
-	}
-
-	/* #endif */
 	.example-body {
-		flex-direction: row;
-		justify-content: flex-start;
+		padding: 0px 12px;
+		background-color: #FFFFFF;
 	}
 
-	.uni-badge-left-margin {
-		margin-left: 20rpx;
+	.result-box {
+		text-align: center;
+		padding: 20px 0px;
+		font-size: 16px;
 	}
 </style>
--- pages/tabBar/Official/Official.vue
+++ pages/tabBar/Official/Official.vue
@@ -145,40 +145,155 @@ const keys = {
   },
   '5002': {
     icon: '../../../static/images/main/icon_5002.png',
-    path: '../machine/machine-lease/machine-lease'
+    path: '../../extUI/calendar/calendar'
   },
   '5003': {
     icon: '../../../static/images/main/icon_5003.png',
-    path: '../safety/safety-check/safety-check'
+    path: '../../extUI/card/card'
   },
   '5004': {
     icon: '../../../static/images/main/icon_5004.png',
-    path: '../safety/question-list/question-list'
+    path: '../../extUI/collapse/collapse'
   },
   '5005': {
     icon: '../../../static/images/main/icon_5005.png',
-    path: '../safety/question-list/question-list'
+    path: '../../extUI/combox/combox'
   },
-  // 质量管理
-  '6001': {
+  '5006': {
+    icon: '../../../static/images/main/icon_5005.png',
+    path: '../../extUI/countdown/countdown'
+  },
+  '5007': {
+    icon: '../../../static/images/main/icon_5005.png',
+    path: '../../extUI/data-checkbox/data-checkbox'
+  },
+  '5008': {
     icon: '../../../static/images/main/icon_6001.png',
-    path: '../quality/patrol-plan/new-plan/new-plan'
+    path: '../../extUI/data-picker/data-picker'
   },
-  '6002': {
+  '5009': {
     icon: '../../../static/images/main/icon_6003.png',
-    path: '../quality/safety-check/safety-check'
+    path: '../../extUI/date-picker/date-picker'
   },
-  '6003': {
+  '5010': {
     icon: '../../../static/images/main/icon_6004.png',
-    path: '../quality/question-list/question-list'
+    path: '../../extUI/dateformat/dateformat'
   },
-  '6004': {
+  '5011': {
     icon: '../../../static/images/main/icon_6005.png',
-    path: '../quality/question-list/question-list'
+    path: '../../extUI/datatime-picker/datatime-picker'
   },
-  '6005': {
+  '5012': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/drawer/drawer'
+  },
+  '5013': {
     icon: '../../../static/images/main/icon_6002.png',
-    path: '../quality/acceptance/acceptance'
+    path: '../../extUI/easyinput/easyinput'
+  },
+  '5014': {
+    icon: '../../../static/images/main/icon_7002.png',
+    path: '../../extUI/fab/fab'
+  },
+  '5015': {
+    icon: '../../../static/images/main/icon_6002.png',
+    path: '../../extUI/fav/fav'
+  },
+  '5016': {
+    icon: '../../../static/images/main/icon_6002.png',
+    path: '../../extUI/file-picker/file-picker'
+  },
+  '5017': {
+    icon: '../../../static/images/main/icon_4001.png',
+    path: '../../extUI/forms/forms'
+  },
+  '5018': {
+    icon: '../../../static/images/main/icon_6002.png',
+    path: '../../extUI/goods-nav/goods-nav'
+  },
+  '5019': {
+    icon: '../../../static/images/main/icon_4002.png',
+    path: '../../extUI/icons/icons'
+  },
+  '5020': {
+    icon: '../../../static/images/main/icon_3002.png',
+    path: '../../extUI/indexed-list/indexed-list'
+  },
+  '5021': {
+    icon: '../../../static/images/main/icon_7005.png',
+    path: '../../extUI/link/link'
+  },
+  '5022': {
+    icon: '../../../static/images/main/icon_6002.png',
+    path: '../../extUI/list/list'
+  },
+  '5023': {
+    icon: '../../../static/images/main/icon_7003.png',
+    path: '../../extUI/load-more/load-more'
+  },
+  '5024': {
+    icon: '../../../static/images/main/icon_7002.png',
+    path: '../../extUI/nav-bar/nav-bar'
+  },
+  '5025': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/notice-bar/notice-bar'
+  },
+  '5026': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/number-box/number-box'
+  },
+  '5027': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/pagination/pagination'
+  },
+  '5028': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/popup/popup'
+  },
+  '5029': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/rate/rate'
+  },
+  '5030': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/row/row'
+  },
+  '5031': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/search-bar/search-bar'
+  },
+  '5032': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/segmented-control/segmented-control'
+  },
+  '5033': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/steps/steps'
+  },
+  '5034': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/swipe-action/swipe-action'
+  },
+  '5035': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/swiper-dot/swiper-dot'
+  },
+  '5036': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/table/table'
+  },
+  '5037': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/tag/tag'
+  },
+  '5038': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/title/title'
+  },
+  '5039': {
+    icon: '../../../static/images/main/icon_7001.png',
+    path: '../../extUI/transition/transition'
   }
 }
 export default {
@@ -327,43 +442,157 @@ export default {
               code: '5001'
             },
             {
-              title: '安全检查',
+              title: '日历',
+              code: '5002'
+            },
+            {
+              title: '卡片',
               code: '5003'
             },
             {
-              title: '问题整改',
+              title: '折叠面板',
               code: '5004'
             },
             {
-              title: '整改验收',
+              title: '组合框',
               code: '5005'
+            },
+            {
+              title: '倒计时',
+              code: '5006'
+            },
+            {
+              title: '数据选择器',
+              code: '5007'
+            },
+            {
+              title: '级联选择',
+              code: '5008'
+            },
+            {
+              title: '日期选择',
+              code: '5009'
+            },
+            {
+              title: '日期格式化',
+              code: '5010'
+            },
+            {
+              title: '时间选择',
+              code: '5011'
+            },
+            {
+              title: '抽屉',
+              code: '5012'
+            },
+            {
+              title: '输入框',
+              code: '5013'
+            },
+            {
+              title: '悬浮按钮',
+              code: '5014'
+            },
+            {
+              title: '收藏按钮',
+              code: '5015'
+            },
+            {
+              title: '文件上传',
+              code: '5016'
+            },
+            {
+              title: '表单',
+              code: '5017'
+            },
+            {
+              title: '商品导航',
+              code: '5018'
+            },
+            {
+              title: 'icons',
+              code: '5019'
+            },
+            {
+              title: '分类列表',
+              code: '5020'
+            },
+            {
+              title: '超链接',
+              code: '5021'
+            },
+            {
+              title: '列表',
+              code: '5022'
+            },
+            {
+              title: '加载',
+              code: '5023'
+            },
+            {
+              title: '导航栏',
+              code: '5024'
+            },
+            {
+              title: '通告栏',
+              code: '5025'
+            },
+            {
+              title: '输入框',
+              code: '5026'
+            },
+            {
+              title: '分页器',
+              code: '5027'
+            },
+            {
+              title: '弹出层',
+              code: '5028'
+            },
+            {
+              title: '评分',
+              code: '5029'
+            },
+            {
+              title: '行布局',
+              code: '5030'
+            },
+            {
+              title: '搜索栏',
+              code: '5031'
+            },
+            {
+              title: '分段器',
+              code: '5032'
+            },
+            {
+              title: '步骤条',
+              code: '5033'
+            },
+            {
+              title: '滑动',
+              code: '5034'
+            },
+            {
+              title: '轮播图',
+              code: '5035'
+            },
+            {
+              title: '表格',
+              code: '5036'
+            },
+            {
+              title: '标签',
+              code: '5037'
+            },
+            {
+              title: '标题',
+              code: '5038'
+            },
+            {
+              title: '过渡',
+              code: '5039'
             }
-          ]
-        },
-        {
-          title: '质量管理',
-          code: '60',
-          child: [
-            {
-              title: '质量计划',
-              code: '6001'
-            },
-            {
-              title: '质量检查',
-              code: '6002'
-            },
-            {
-              title: '问题整改',
-              code: '6003'
-            },
-            {
-              title: '整改验收',
-              code: '6004'
-            },
-			{
-        title: '质量验收',
-        code: '6005'
-			}
           ]
         }
       ]
